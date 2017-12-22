2725b0d8b20d894944c72174121e4977fcdd769e
Ammar Faizi <ammarfaizi2@gmail.com>
2017-12-22 20:14:29 +0700

    Fix

--- get_info.php
+++ get_info.php
@@ -2,5 +2,17 @@
 
 function get_info($fn)
 {
-
+	$fn = new ReflectionFunction($fn);
+	$i = 0;
+	foreach ($fn->getParameters() as $key => $val) {
+		strpos($val, "required") !== false and $i++;
+	}
+	return [
+		$key + 1,
+		$i,
+		$fn->hasReturnType(),
+		$fn->isClosure(),
+		$fn->isInternal(),
+		$fn->isUserDefined()
+	];
 }
